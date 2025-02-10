ce63a9947330e1c55e531484a1fc3225665484f2
Yudhi <baskyudh@gmail.com>
2025-02-10 14:13:03 +0700

    Add hover rules - Random color assigned to hovered box - Random color assigned to hovered box after grid resize - To get darkenColor to work

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-// set randomColor Math.floor(Math.random()*16777215).toString(16)
-// extra credit
-// create function to darken color
-// add mouseenter event listener to container, change target background color to "#" + randomColor (which is Math.floor(Math.random()*16777215).toString(16))
-// set interval to run darken color every second
-// clear interval on mouseleave
-
 const body = document.querySelector("body");
 const size = document.querySelector(".size");
 let sizeVal = 16;
 const changeBtn = document.querySelector(".change-button");
 const container = document.querySelector(".container");
+const box = document.querySelector(".box");
 const column = document.querySelector(".column");
-const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-const box = `<div class=box></div>`;
-column.innerHTML = box.repeat(sizeVal);
+column.innerHTML = `<div class=box></div>`.repeat(sizeVal);
 container.innerHTML = container.innerHTML.repeat(sizeVal);
+let interval;
+
+let darkenColor = (hex, amount) => {
+    let num = parsenInt(hex, 16);
+    let r = Math.max((num >> 16) - amount, 0);
+    let g = Math.max(((num >> 8) & Ox00FF )- amount, 0);
+    let b = Math.max((num & 0x0000FF) - amount, 0);
+    return `#${(r << 16 | g << 8 | b).toString(16)}`
+}
 
 changeBtn.addEventListener("click", () => {
   if (size.value >= 16 && size.value <= 100) {
     sizeVal = size.value;
-    // container.innerHTML = "";
     container.innerHTML = `<div class="column"></div>`
-    document.querySelector(".column").innerHTML = `<div class=box></div>`.repeat(sizeVal);
-    container.innerHTML = container.innerHTML.repeat(sizeVal);
+    document.querySelector(".column").innerHTML = `<div class=box></div>`.repeat(sizeVal); // create columns
+    container.innerHTML = container.innerHTML.repeat(sizeVal); // create rows
+    document.querySelectorAll(".box").forEach(box => {
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16).padStart(6,'0');
+        box.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = `#${randomColor}`
+        });
+        interval = setInterval(()=>{
+            randomColor = darkenColor(randomColor, 15);
+            box.style.backgroundColor = randomColor;
+        }, 1000)
+    });
   } else {
     alert("Value must be between 16 and 100");
   }
 });
+
+document.querySelectorAll(".box").forEach(box => {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16).padStart(6,'0');
+    box.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = `#${randomColor}`
+    });
+});
+
+container.addEventListener("mouseleave", (e) => {
+    clearInterval(interval);
+})
